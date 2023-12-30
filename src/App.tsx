308ae9520a7a1188dc00ba0b0cfc652b14bd89a2
Henry Tochukwu Uzor <h.uzor17@gmail.com>
2023-12-30 22:56:25 +0100

    feat: oscar props imported and mounted in App

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { Person } from './components/Person';
 import { PersonList } from './components/PersonList';
 import { Status } from './components/Status';
 import { Heading } from './components/Heading';
+import { Oscar } from './components/Oscar';
 
 function App() {
   const PersonName = {
@@ -49,6 +50,9 @@ function App() {
       <PersonList names={nameList}/>
       <Status status='error' />
       <Heading>Placeholder text</Heading>
+      <Oscar>
+        <Heading>Oscar goes to Leonardo Dicaprio!</Heading>
+      </Oscar>
     </div>
   );
 }
